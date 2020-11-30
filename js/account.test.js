ab8aa74a30a4af94cfc024dbd72de0646b513728
Kiril <kiril@olgas-air.mynet>
2020-11-30 14:28:54 +0000

    withdrawal test and passing code

--- js/account.test.js
+++ js/account.test.js
@@ -6,7 +6,12 @@ test('has a balance of 0 when created', () => {
   expect(testAccount.balance).toBe(0);
 });
 
-test('depositing £500 changes the balance by 500', () => {
+test('depositing cash increases the balance', () => {
   testAccount.deposit(500);
   expect(testAccount.balance).toBe(500);
+});
+
+test('withdrawing cash decreases the balance', () => {
+  testAccount.withdraw(300);
+  expect(testAccount.balance).toBe(200);
 });
