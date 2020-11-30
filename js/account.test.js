913889246a7a84849e3aed34b02638fc3f83e57c
Kiril <kiril@olgas-air.mynet>
2020-11-30 14:23:06 +0000

    test for account deposit and passing code

--- js/account.test.js
+++ js/account.test.js
@@ -5,3 +5,8 @@ const testAccount = new Account();
 test('has a balance of 0 when created', () => {
   expect(testAccount.balance).toBe(0);
 });
+
+test('depositing £500 changes the balance by 500', () => {
+  testAccount.deposit(500);
+  expect(testAccount.balance).toBe(500);
+});
